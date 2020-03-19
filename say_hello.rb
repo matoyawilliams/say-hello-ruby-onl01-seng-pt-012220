f8b3adcabca80269425f844ea52176d5d8ee619f
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-19 00:51:24 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,8 +2,8 @@ def say_hello(name)
   puts "Hello #{name}."
 end
 
-def say_hello("") = "Ruby programmer"
-  puts "Hello, Ruby Programmer"
+def say_hello(name) = "Ruby programmer"
+  puts "Hello, #{name}."
 end
 
 
